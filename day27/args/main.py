9edaea92d783659776e79646c2149c08038af43d
Craig <cjcs@me.com>
2022-12-28 12:49:16 -0800

    tkinter

--- day27/args/main.py
+++ day27/args/main.py
@@ -1,7 +1,26 @@
-
-
 def add(*args):
     return sum(args)
 
 
+def calculate(n, **kwargs):
+    if "add" in kwargs:
+        n += kwargs["add"]
+    if "multiply" in kwargs:
+        n *= kwargs["multiply"]
+    return n
+
+
 print(add(1, 2, 3, 4))
+print(calculate(2, add=3, subtract=10))
+
+
+class Car:
+
+    def __init__(self, **kw):
+        self.make = kw.get("make")
+        self.model = kw.get("model")  # get() works like a square bracket, but no error if missing
+
+
+my_car = Car(make="Subaru")
+print(my_car.model)
+
--- day27/tkinter/main.py
+++ day27/tkinter/main.py
@@ -1,16 +1,36 @@
 import tkinter
 
+
+def button_clicked():
+    new_text = my_input.get()
+    if new_text != "":
+        my_label["text"] = new_text
+
+
 window = tkinter.Tk()
 window.title("My First GUI Program")
 window.minsize(width=500, height=300)
+window.config(padx=100, pady=200)
 
+# Label
 my_label = tkinter.Label(text="I am a label", font=("Arial", 24, "bold"))
-my_label.pack(side="left")  # needed to make label show up on screen
+my_label["text"] = "New Text"
+my_label.config(text="Newer Text")
+# my_label.pack()  # pack is too opaque, it puts things together for you
+# my_label.place(x=100, y=200)  # place is too specific, requires exact coordinates
+my_label.grid(column=0, row=0)  # cannot use both grid and pack in same program
+my_label.config(padx=50, pady=50)
 
+# Button
+my_button = tkinter.Button(text="Click me!", command=button_clicked)
+my_button.grid(column=1, row=1)
 
+new_button = tkinter.Button(text="New Button", command=button_clicked)
+new_button.grid(column=2, row=0)
 
-
-
-
+# Entry
+my_input = tkinter.Entry(width=10)
+my_input.grid(column=3, row=2)
 
 window.mainloop()  # creates a while loop that keeps the window running and awaiting instruction
+
